921c0270b5dd4272e3dd28233a615fb6a4bc8b73
Katarzyna Sekowska <sekowska.k@gmail.com>
2018-04-14 11:53:03 +0200

    Added storage options

--- photographer/photographer/HttpTrigger.cs
+++ photographer/photographer/HttpTrigger.cs
@@ -6,6 +6,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace photographer
 {
@@ -13,24 +14,34 @@ namespace photographer
     {
         [FunctionName("HttpTrigger")]
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "order")]HttpRequest req,
-            TraceWriter log)
+            [Table("Orders", Connection = "StorageConnection")] ICollector<PhotoOrder> ordersTable, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            string email = data?.CustomerEmail;
-            string photoWidth = data?.RequiredWidth;
-            string photoHeight= data?.RequiredHeight;
-            string photoName = data?.FileName;
-
-            if (data == null || email == null || photoWidth == null || photoHeight == null ||  photoName == null)
+            try
             {
-                return new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+                string requestBody = new StreamReader(req.Body).ReadToEnd();
+                PhotoOrder orderData = JsonConvert.DeserializeObject<PhotoOrder>(requestBody);
+                orderData.PartitionKey = System.DateTime.UtcNow.DayOfYear.ToString();
+                orderData.RowKey = orderData.FileName;
+                ordersTable.Add(orderData);
+            }
+            catch (System.Exception ex)
+            {
+                log.Error("Something went wrong", ex);
+                return new BadRequestObjectResult("Something went wrong");
             }
 
-            return (ActionResult)new OkObjectResult("OK");
-          
+            return (ActionResult)new OkObjectResult($"Order processed");
+
         }
+    }
+
+    public class PhotoOrder : TableEntity
+    {
+        public string CustomerEmail { get; set; }
+        public string FileName { get; set; }
+        public int RequiredHeight { get; set; }
+        public int RequiredWidth { get; set; }
     }
 }
